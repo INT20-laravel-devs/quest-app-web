6a8bc64d85155143c2e9f38f31ce3897cc05cb4b
igaryakqwe <igor.novykov@gmail.com>
2025-02-12 19:40:21 +0200

    feat: integrate update user

--- src/features/profile/components/update-form.tsx
+++ src/features/profile/components/update-form.tsx
@@ -13,27 +13,77 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { UpdateFormInputs, updateSchema } from './update-form-schema';
+import { type UpdateFormInputs, updateSchema } from './update-form-schema';
+import ImageUpload from '@/components/image-upload';
+import useAuthStore from '@/store/use-auth-store';
+import { useEffect } from 'react';
+import { updateAvatar, updateProfile } from '@/api/user';
+import { toast } from 'sonner';
+
+async function createFileFromUrl(
+  url: string,
+  fileName: string,
+  mimeType: string,
+): Promise<File> {
+  const response = await fetch(url);
+  const data = await response.blob();
+  return new File([data], fileName, { type: mimeType });
+}
 
 const UpdateForm = () => {
+  const user = useAuthStore((state) => state.user);
   const form = useForm<UpdateFormInputs>({
     resolver: zodResolver(updateSchema),
     defaultValues: {
-      nickname: '',
-      password: '',
+      nickname: user?.nickname,
     },
   });
 
-  function onSubmit(values: UpdateFormInputs) {
-    console.log(values);
+  useEffect(() => {
+    if (!user?.avatarLink) return;
+
+    createFileFromUrl(user?.avatarLink, 'avatar.png', 'image/png').then(
+      (file) => {
+        form.setValue('profileImage', file);
+      },
+    );
+  }, [user]);
+
+  async function onSubmit(values: UpdateFormInputs) {
+    try {
+      const formData = new FormData();
+      await updateProfile(user?.id as string, values.nickname);
+
+      if (values.profileImage) {
+        formData.append('file', values.profileImage);
+        await updateAvatar(formData);
+      }
+      toast.success('Profile updated successfully');
+    } catch (e) {
+      if (e instanceof Error) {
+        toast.error(e.message);
+      }
+      console.error(e);
+    }
   }
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className=" container space-y-8"
+        className="container space-y-8"
       >
+        <FormField
+          control={form.control}
+          name="profileImage"
+          render={({ field }) => (
+            <ImageUpload
+              onChange={(file) => field.onChange(file)}
+              imageLink={user?.avatarLink}
+            />
+          )}
+        />
+
         <FormField
           control={form.control}
           name="nickname"
@@ -49,28 +99,11 @@ const UpdateForm = () => {
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Enter your password"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                Must be at least 1 characters long.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button type="submit" className="w-full">
+        <Button
+          type="submit"
+          className="w-full"
+          isLoading={form.formState.isSubmitting}
+        >
           Update Details
         </Button>
       </form>
--- src/features/profile/profile-page.tsx
+++ src/features/profile/profile-page.tsx
@@ -4,8 +4,11 @@ import { FC } from 'react';
 import UpdateForm from './components/update-form';
 import { Hero } from '@/components/common/hero';
 import { Cta } from '@/components/common/cta';
+import useAuthStore from '@/store/use-auth-store';
 
 const ProfilePage: FC = () => {
+  const user = useAuthStore((state) => state.user);
+
   return (
     <>
       <Hero
@@ -19,7 +22,7 @@ const ProfilePage: FC = () => {
       <div className="container max-w-5xl ">
         <div className="px-4 grid gap-12 md:grid-cols-[1fr,400px] items-center">
           <Cta />
-          <UpdateForm />
+          {user && <UpdateForm />}
         </div>
       </div>
     </>
